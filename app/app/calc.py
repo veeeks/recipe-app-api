5874ebd25cef2e6cc158a7d805f0eeb080fec356
venkatesh k <venkatesh31999@gmail.com>
2024-08-26 07:24:52 +0530

    Added Wait for DB function

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-
-Calculator function.
-
+Calculator functions
 """
 
 
-def add (x,y):
-   return x+y 
-def subtract(x,y):
-   return y-x
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
